e44354f0f51635afaf09d02694331972e71451d0
Muhammed Yusuf <myusuf2121@gmail.com>
2022-05-08 21:57:29 +0300

    Configurations to solve webpack polyfill errors.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-});
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()]
+  }
+})
